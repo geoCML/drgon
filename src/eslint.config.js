051edfa58e3b5f334c550fd990eb9e9eaee4f87f
Tristan Damron <tristandamron@protonmail.com>
2024-12-07 11:05:28 -0800

    Adopt eslint

---
+++ src/eslint.config.js
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-prototype-builtins": "off"
+        }
+    }
+];
